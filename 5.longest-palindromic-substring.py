706c7e2a97de4f2a8079ae587fc0a7c09c513a4c
Karan Rajbhar <kran@outlook.com>
2023-08-11 10:03:03 +0530

    solve q 5

--- 5.longest-palindromic-substring.py
+++ 5.longest-palindromic-substring.py
@@ -4,9 +4,32 @@
 # [5] Longest Palindromic Substring
 #
 
+
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-# @lc code=end
+        longestPalindromeSubString = ""
+        if len(s) == 1:
+            longestPalindromeSubString = s
+        if self.checkIfPalindrome(s):
+            return s
 
+        for first_index in range(0, len(s)+1):
+            for second_index in range(0, len(s)+1):
+                current_string = s[first_index:second_index]
+                if (
+                    self.checkIfPalindrome(current_string)
+                    and len(current_string) > len(longestPalindromeSubString)
+                    
+                ):
+                    longestPalindromeSubString = current_string
+
+        return longestPalindromeSubString
+
+    def checkIfPalindrome(self, string: str):
+        if string == string[::-1]:
+            return True
+        return False
+    
+
+# @lc code=end
