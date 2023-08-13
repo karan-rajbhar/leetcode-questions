739e1777da0ef6691ea0cf5441f224d7a0b2ee13
Karan Rajbhar <kran@outlook.com>
2023-08-13 19:02:07 +0530

    problem 6 completed

--- 6.zigzag-conversion.py
+++ 6.zigzag-conversion.py
@@ -7,25 +7,23 @@
 # @lc code=start
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        num_next = numRows-2
-        array=[]
-        count=0
-        column=[]
-        for index in(0, len(s)+1) :
+        if numRows == 1 or len(s)==1:
+            return s
+        
+        string=['']* numRows
+        index=0
+        step=1
+        for char in s:
+            string[index] = string[index]+char
+            if index ==0:
+                step =1
+            elif index == numRows-1:
+                step=-1
+            index = index +step
+        
+        return ''.join(string)
             
-            if count != numRows:
-                column.append(s[index])
-                count= count+1
-
-            if count== numRows:
-                array.append(column)
-                column = []
-                count = 0
-
+                     
             
-            
-            
-            
-
 # @lc code=end
 
