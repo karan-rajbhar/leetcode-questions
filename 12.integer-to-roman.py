dcf3d70cfe1ebb61bd09bd60bddd023598893e16
Karan Rajbhar <kran@outlook.com>
2023-08-20 22:23:48 +0530

    solved question 12 and 13

--- 12.integer-to-roman.py
+++ 12.integer-to-roman.py
@@ -8,39 +8,48 @@
 # @lc code=start
 class Solution:
     def intToRoman(self, num: int) -> str:
-        roman_map = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
-        num=1994
-
-
+        roman_dict = {
+            0:"",
+            1: "I",
+            2: "II",
+            3: "III",
+            4: "IV",
+            5: "V",
+            6: "VI",
+            7: "VII",
+            8: "VIII",
+            9: "IX",
+            10: "X",
+            20: "XX",
+            30: "XXX",
+            40: "XL",
+            50: "L",
+            60: "LX",
+            70: "LXX",
+            80: "LXXX",
+            90: "XC",
+            100: "C",
+            200: "CC",
+            300: "CCC",
+            400: "CD",
+            500: "D",
+            600: "DC",
+            700: "DCC",
+            800: "DCCC",
+            900: "CM",
+            1000: "M",
+            2000: "MM",
+            3000: "MMM",
+        }
         tens_place=1
-        word_list=[]
-        last_digit_list=[]
-        while True:
+        wordList=[]
+        while num > 0:
             last_digit=num%10
-            last_digit_list.append(last_digit*tens_place)
+            last_digit_place = last_digit*tens_place 
+            wordList.append(roman_dict[last_digit_place])
             tens_place = tens_place*10
             num=int(num/10)
-            if num <= 0:
-                break
-
-        for number in last_digit_list[::-1]:
-            if number == 0:
-                continue
             
-            if number in roman_map.keys():
-                word_list.append(roman_map.get(number))
-                continue
-
-            if number > 1000 :
-                word_list.append('M' * (number/1000))
-            elif number >500 and number < 1000:
-                x=1000-number
-
-
-
-            
-
-
-    
+        return ''.join(wordList[::-1])
 
 # @lc code=end
---
+++ 13.roman-to-integer.py
@@ -0,0 +1,70 @@
+#
+# @lc app=leetcode id=13 lang=python3
+#
+# [13] Roman to Integer
+#
+
+# @lc code=start
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dict = {
+            0:"",
+            1: "I",
+            2: "II",
+            3: "III",
+            5: "V",
+            6: "VI",
+            7: "VII",
+            8: "VIII",
+            10: "X",
+            20: "XX",
+            30: "XXX",
+            50: "L",
+            60: "LX",
+            70: "LXX",
+            80: "LXXX",
+            100: "C",
+            200: "CC",
+            300: "CCC",
+            500: "D",
+            600: "DC",
+            700: "DCC",
+            800: "DCCC",
+            1000: "M",
+            2000: "MM",
+            3000: "MMM",
+            4: "IV",
+            9: "IX",
+            40: "XL",
+            90: "XC",
+            400: "CD",
+            900: "CM",
+        }
+
+        
+        num_list=[]
+        dictionary_items=list(roman_dict.items())
+        for key, value in dictionary_items[::-1]:
+            if value in s:
+                num_list.append(value)
+                s=s.replace(value,'.')
+    
+        reversed_dictionary={}
+        for key , value in roman_dict.items():
+            reversed_dictionary[value]=key
+
+        num=0
+        for roman_num in num_list:
+            num_value = reversed_dictionary[roman_num]
+            num= num+num_value
+
+        return num
+
+
+            
+
+
+solution = Solution()
+print(solution.romanToInt('MCMXCIV'))
+# @lc code=end
+
