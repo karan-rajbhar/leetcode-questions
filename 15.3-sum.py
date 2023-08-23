e6390550755f6a8b9fb6cdfb8a3fd7cbba49e83b
Karan Rajbhar <kran@outlook.com>
2023-08-23 10:03:34 +0530

    started question no 15

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -5,8 +5,39 @@
 #
 
 # @lc code=start
+
+from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+            threeSumList = []
+            numberOfTriplets = len(nums)//3
+            indexPositionDict={}
+            for i in range(1, numberOfTriplets+1):
+                triplets = []
+                index = 0
+                loopBreak=True
+                
+                while loopBreak:
+                    print(len(triplets))
+                    if len(triplets) == 3 and sum(triplets) == 0:
+                        loopBreak=False
+                    if triplets == []:
+                        triplets.append(nums[index])
+                        index+=1
+                        
+                    if len(triplets) == 2:
+                        remaining = 0 - sum(triplets)
+                        if nums[index] == remaining:
+                            triplets.append(nums[index])
+                    
+                    if len(triplets) == 1:
+                        triplets.append(nums[index])
+
+                    index+=1
+                    
+                        
+                threeSumList.append(triplets)
+                
+            return threeSumList        
 # @lc code=end
 
