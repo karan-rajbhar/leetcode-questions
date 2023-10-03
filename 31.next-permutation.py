5cac96dcd770f1963bb10bd57cb84c589a669e91
Karan Rajbhar <kran@outlook.com>
2023-10-03 17:19:47 +0530

    added brut  approach

--- 31.next-permutation.py
+++ 31.next-permutation.py
@@ -3,7 +3,8 @@
 #
 # [31] Next Permutation
 #
-
+from itertools import permutations
+from typing import List
 # @lc code=start
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
@@ -11,6 +12,20 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
+    def nextPermutationBrutForce(self,nums: List[int]) :
+        sorted_nums= sorted(nums)
+        
+        combinations =  list(permutations(sorted_nums))
+        for index ,permutation in enumerate(combinations):
+            combo= list(permutation)
+            if combo == nums:
+                if index == len(combinations)-1:
+                    return combinations[0]
+                return combinations[index+1]
+        
+
+solution = Solution()
+print(solution.nextPermutationBrutForce([3,2,1]))
         
 # @lc code=end
 
