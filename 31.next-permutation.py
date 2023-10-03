c5ac17b5b5d67620a27f5d4d8426474fb8b6620c
Karan Rajbhar <kran@outlook.com>
2023-10-03 19:15:29 +0530

    added brut approach for problem number 31

--- 31.next-permutation.py
+++ 31.next-permutation.py
@@ -7,25 +7,28 @@ from itertools import permutations
 from typing import List
 # @lc code=start
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
+    def nextPermutationBrutForce(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         
-    def nextPermutationBrutForce(self,nums: List[int]) :
+    def nextPermutation(self,nums: List[int]) :
         sorted_nums= sorted(nums)
+        combinations =  list(dict.fromkeys(list(permutations(sorted_nums))))        
         
-        combinations =  list(permutations(sorted_nums))
         for index ,permutation in enumerate(combinations):
             combo= list(permutation)
             if combo == nums:
                 if index == len(combinations)-1:
-                    return combinations[0]
-                return combinations[index+1]
-        
+                    nums[:] = list(combinations[0])
+                    break
+                nums[:] = list(combinations[index+1])
+                break
 
-solution = Solution()
-print(solution.nextPermutationBrutForce([3,2,1]))
-        
+
+# solution = Solution()
+# nums=[1,5,1]
+# solution.nextPermutation(nums)
+# print(nums)        
 # @lc code=end
 
