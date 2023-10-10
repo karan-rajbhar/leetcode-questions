5e7228d216d2b15608b8794f5708315dc656f3b1
Karan Rajbhar <kran@outlook.com>
2023-10-10 10:54:30 +0530

    some changes

--- 32.longest-valid-parentheses.py
+++ 32.longest-valid-parentheses.py
@@ -8,26 +8,25 @@
 # @lc code=start
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        stack = []
-        max_length = 0
-        start = -1
+        stack=[]
+        max_length=0
+        start=-1
 
         for i in range(len(s)):
-            print(start)
             if s[i] == '(':
                 stack.append(i)
             else:
                 if not stack:
-                    start = i
+                    start=i
                 else:
                     stack.pop()
                     if not stack:
-                        max_length = max(max_length, i - start)
+                        max_length=max(max_length, i- start)
                     else:
-                        max_length = max(max_length, i - stack[-1])
-
+                        max_length= max(max_length, i - stack[-1])
         return max_length
 
-solution = Solution()
-print(solution.longestValidParentheses(s=")()())"))
+
+# solution = Solution()
+# print(solution.longestValidParentheses(s=")()())"))
 # @lc code=end
