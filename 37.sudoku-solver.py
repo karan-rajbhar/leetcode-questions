d60012eeeea21320fd51de2a049bafe8c23f61c1
Karan Rajbhar <kran@outlook.com>
2023-10-18 21:27:41 +0530

    backtracking method

--- 37.sudoku-solver.py
+++ 37.sudoku-solver.py
@@ -3,6 +3,7 @@
 #
 # [37] Sudoku Solver
 #
+from typing import List
 
 # @lc code=start
 class Solution:
@@ -10,6 +11,13 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        while True:
         
+
+board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+
+solution= Solution()
+solution.solveSudoku(board=board)
+
 # @lc code=end
 
