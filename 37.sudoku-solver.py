db6f92ecb9c195b60df18dae8bfd0158d1e2a569
Karan Rajbhar <kran@outlook.com>
2023-10-19 22:01:49 +0530

    small improvments in sudoku solver

--- 37.sudoku-solver.py
+++ 37.sudoku-solver.py
@@ -11,8 +11,39 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        while True:
+        pass
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Check Row
+        for row in board:
+            rowDict = {}
+            for num in row:
+                if rowDict.get(num, None):
+                    return False
+                if num != ".":
+                    rowDict[num] = True
+
+        # Check Column
+        for i in range(len(board)):
+            columnDict = {}
+            for j in range(len(board[i])):
+                if columnDict.get(board[j][i], None):
+                    return False
+                if board[j][i] != ".":
+                    columnDict[board[j][i]] = True
         
+        # Check Square
+        for i in range(0,len(board),+3):
+            for j in range(0,len(board),+3):
+                gridDict={}
+                for row_index in range(i,i+3):
+                    for column_index in range(j,j+3): 
+                        if gridDict.get(board[row_index][column_index], None):
+                            return False
+                        if board[row_index][column_index] != ".":
+                            gridDict[board[row_index][column_index]] = True
+        return True
+
 
 board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 
