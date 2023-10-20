2f59f1b1b6731c654b4dd63a689acbab64a004c7
Karan Rajbhar <kran@outlook.com>
2023-10-20 13:32:01 +0530

    solved sudoku solver

--- 37.sudoku-solver.py
+++ 37.sudoku-solver.py
@@ -10,8 +10,34 @@ class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
+
         """
-        pass
+        empty_location=[0,0]
+
+        if (not self.find_empty_location(board,empty_location)):
+            return True
+        
+        row= empty_location[0]
+        column = empty_location [1]
+
+        for num in range(1,10):
+            board[row][column]=str(num)
+            if self.isValidSudoku(board):
+                if self.solveSudoku(board):
+                    return True
+                board[row][column]='.'
+            else:
+                board[row][column]='.'
+        return False
+    
+    def find_empty_location(self,board,empty_location):
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    empty_location[0]=i
+                    empty_location[1]=j
+                    return True
+        return False
 
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         # Check Row
@@ -45,10 +71,11 @@ class Solution:
         return True
 
 
-board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+# board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 
-solution= Solution()
-solution.solveSudoku(board=board)
+# solution= Solution()
+# solution.solveSudoku(board)
+# print(board)
 
 # @lc code=end
 
