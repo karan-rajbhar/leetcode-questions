b82a52eada8a56adb1eeeae31dedc657c18e6407
Karan Rajbhar <kran@outlook.com>
2023-10-25 20:05:01 +0530

    added solution for problem number 39

--- 39.combination-sum.py
+++ 39.combination-sum.py
@@ -3,10 +3,16 @@
 #
 # [39] Combination Sum
 #
-
+from typing import List
 # @lc code=start
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.ans = []                                  
+        def traverse(candid, arr,sm):                  
+            if sm == target: self.ans.append(arr)      
+            if sm >= target: return                    
+            for i in range(len(candid)):               
+                traverse(candid[i:], arr + [candid[i]], sm+candid[i])   
+        traverse(candidates,[], 0)
         
-# @lc code=end
-
+        return self.ans
