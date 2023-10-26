8615866fda3b0a136d6fdbfbe9b8f09c2ec517d5
Karan Rajbhar <kran@outlook.com>
2023-10-26 21:57:40 +0530

    better code

--- 40.combination-sum-ii.py
+++ 40.combination-sum-ii.py
@@ -7,19 +7,18 @@ from typing import List
 # @lc code=start
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-
-        self.ans = []                                  
+        candidates.sort()
+        self.ans = []                                 
         def traverse(candid, arr,sm):
-            if sm == target: self.ans.append(arr)                    
+            if sm == target :
+                self.ans.append(arr)                    
             if sm >= target: return                    
-            for i in range(len(candid)):               
-                traverse(candid[i+1:], arr + [candid[i]], sm+candid[i])   
+            for i in range(len(candid)):
+                if i==0 or candid[i] != candid[i-1]:               
+                    traverse(candid[i+1:], arr + [candid[i]], sm+candid[i])   
         traverse(candidates,[], 0)
-        sorted_ans=[sorted(item) for item in self.ans]
 
-        ans=[]
-        [ans.append(item) for item in sorted_ans if item not in ans]
-        return ans
+        return self.ans
     
 # solution = Solution()
 # print(solution.combinationSum2([10,1,2,7,6,1,5],8))
