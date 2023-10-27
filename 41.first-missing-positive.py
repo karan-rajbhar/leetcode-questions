e9c49f216c3ebb41092bd70b47eb58347663b4a9
Karan Rajbhar <kran@outlook.com>
2023-10-27 08:49:17 +0530

    added approach with O(n*logN)

--- 41.first-missing-positive.py
+++ 41.first-missing-positive.py
@@ -7,6 +7,15 @@ from typing import List
 # @lc code=start
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        smallest_int=1
+        nums.sort()
+        for num in nums:
+            if num == smallest_int:
+                smallest_int +=1
+        
+        return smallest_int
         
 # @lc code=end
 
+# solution= Solution()
+# print(solution.firstMissingPositive([2,1,0]))
