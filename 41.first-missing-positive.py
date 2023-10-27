4f1b533ccf3dee48f83a03d202d40ecf84bd1e95
Karan Rajbhar <kran@outlook.com>
2023-10-27 08:39:28 +0530

    started solving problem number 41

---
+++ 41.first-missing-positive.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=41 lang=python3
+#
+# [41] First Missing Positive
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        
+# @lc code=end
+
