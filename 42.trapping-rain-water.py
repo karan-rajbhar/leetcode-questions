3de9bb03b905d1ac706fd7a0256b9896a50f4afa
Karan Rajbhar <kran@outlook.com>
2023-10-30 10:18:05 +0530

    added function for problem

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -1,4 +1,15 @@
 from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
+        points=self.find_gaps(height)
+        result=0
+
+        for point in points:
+            result +=self.calculate_rain(point[0], point[1])
         
+        return result
+    def find_gaps(self,height:List[int]):
+        pass
+
+    def calculate_rain(self,point_one, point_two):
+        pass
