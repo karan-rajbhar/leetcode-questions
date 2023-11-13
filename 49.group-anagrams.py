7066043c36c2647054ad008543b94ec923840c85
Karan Rajbhar <kran@outlook.com>
2023-11-13 12:29:19 +0530

    solved problem number 49

---
+++ 49.group-anagrams.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=49 lang=python3
+#
+# [49] Group Anagrams
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        result = {}
+
+        for s in strs:
+            temp="".join(sorted(s))
+            
+            if temp in result:
+                result[temp].append(s)
+            else:
+                result[temp] = [s]
+
+        return result.values()
+
+
+# solution = Solution()
+# solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+# @lc code=end
+
