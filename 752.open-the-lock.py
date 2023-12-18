34566ae08afcc216fbcb4aeb413d15c43c0f5f79
Karan Rajbhar <kran@outlook.com>
2023-12-18 18:05:15 +0530

    solved problem  number 752

---
+++ 752.open-the-lock.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode id=752 lang=python3
+#
+# [752] Open the Lock
+#
+from collections import deque
+from typing import List
+
+# @lc code=start
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if '0000' in deadends:
+            return -1
+
+        def children(lock):
+            res=[]
+            for i in range(4):
+                x= int(lock[i])
+                for d in (-1,1):
+                    y=(x+d)%10
+                    res.append(lock[:i]+str(y)+lock[i+1:])
+
+            return res
+        
+        visited= set(deadends)
+        queue= deque()
+        queue.append(['0000',0])
+
+        while queue:
+            lock , step = queue.popleft()
+            if lock == target:
+                return step
+            
+            for child in children(lock):
+                if child not in visited:
+                    visited.add(child)
+                    queue.append([child,step+1])
+
+        return -1        
+    
+
+
+# solution = Solution()
+# print(solution.openLock(["0201","0101","0102","1212","2002"], "0202"))
+        
+# @lc code=end
+
