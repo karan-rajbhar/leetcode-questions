c537e6c82ea80afd7f3677970a68f4f48c015fdf
Karan Rajbhar <kran@outlook.com>
2023-12-28 00:29:05 +0530

    rewritten problem number 2259

--- 2259.remove-digit-from-number-to-maximize-result.py
+++ 2259.remove-digit-from-number-to-maximize-result.py
@@ -7,20 +7,11 @@
 # @lc code=start
 class Solution:
     def removeDigit(self, number: str, digit: str) -> str:
-        index_list=[]
-        for i in range(0, len(number)):
-            if number[i]==digit:
-                index_list.append(i)
-
-        result=number.replace(digit, '' ,1)
-
-        for i in range(len(index_list)):
-            list_text= list(number)
-            del list_text[index_list[i]]
-            new_num ="".join(list_text)
-
-            if int(result) < int(new_num):
-                result= new_num
-        return result        
+        for i in range(len(number)-1):
+            if number[i] == digit:
+                if number[i+1] > number[i]:
+                    return number[:i] + number[i+1:]
+        
+        return number[:number.rindex(digit)]+number[number.rindex(digit)+1:]
 # @lc code=end
 
