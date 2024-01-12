651066d62f8851e8e56ecb00a5e0739b65a666e7
Karan Rajbhar <kran@outlook.com>
2024-01-12 21:34:31 +0530

    solved problem number 713

---
+++ 2825.make-string-a-subsequence-using-cyclic-increments.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2825 lang=python3
+#
+# [2825] Make String a Subsequence Using Cyclic Increments
+#
+
+# @lc code=start
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        
+# @lc code=end
+
---
+++ 713.subarray-product-less-than-k.py
@@ -0,0 +1,51 @@
+#
+# @lc app=leetcode id=713 lang=python3
+#
+# [713] Subarray Product Less Than K
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        count = 0
+        if set(nums) == {1}:
+            if k == 1:
+                return 0
+            if k == 0:
+                return 0
+            else:
+                return len(nums) * (len(nums) + 1) // 2
+        
+        for i in range(len(nums)):
+            product = 1
+            curr_index = i
+            while curr_index < len(nums):
+                product *= nums[curr_index]
+                if product < k:
+                    count += 1
+                    
+                if product >= k:
+                    break
+                curr_index += 1
+            
+        return count
+
+
+    # Brute force solution
+    def numSubarrayProductLessThanKBruteForceSolution(self, nums: List[int], k: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                product = 1
+                
+                for num in nums[i:j+1]:
+                    product *= num
+                if product < k:
+                    count += 1
+        
+        return count
+
+# @lc code=end
+
+# solution = Solution()
+# print(solution.numSubarrayProductLessThanK([10,5,2,6], 100))
