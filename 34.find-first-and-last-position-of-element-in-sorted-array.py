43d99ae91062659f8acdb99e4b610ddf15f3e0f6
Karan Rajbhar <kran@outlook.com>
2024-01-19 15:12:56 +0530

    small improvements

--- 34.find-first-and-last-position-of-element-in-sorted-array.py
+++ 34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -10,72 +10,48 @@ from typing import List
 # @lc code=start
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        if len(nums)==0:
-            return [-1,-1]
-        
+        if len(nums) == 0:
+            return [-1, -1]
+
         if len(nums) == 1:
-            if nums[0]==target:
-                return [0,0]
-            return [-1,-1]
-            
-        
-        left , right = 0, len(nums)-1
-        
-        while left +1 < right:
-            mid =(left+right)//2
-            
+            if nums[0] == target:
+                return [0, 0]
+            return [-1, -1]
+
+        left, right = 0, len(nums) - 1
+
+        while left + 1 < right:
+            mid = (left + right) // 2
+
             if nums[mid] == target:
-                first=mid
-                last=mid
-                while (first-1)>=0:
-                    if nums[first-1]==target:
-                        first=first-1
-                    else:
-                        break
-                while last+1<=len(nums)-1:
-                    if nums[last+1] == target:
-                        last = last+1
-                    else:
-                        break
-                return [first,last]
-            elif nums[mid]<target:
-                left=mid
+                return self.find_first_and_last(mid, nums, target)
+            elif nums[mid] < target:
+                left = mid
             else:
-                right= mid
-         
+                right = mid
+
         if nums[left] == target:
-            first=left
-            last=left
-            while (first-1)>=0:
-                if nums[first-1]==target:
-                    first=first-1
-                else:
-                    break
-            while last+1<=len(nums)-1:
-                if nums[last+1] == target:
-                    last = last+1
-                else:
-                    break
-            return [first,last]
-            
-        
+            return self.find_first_and_last(left, nums, target)
+
         if nums[right] == target:
-            first=right
-            last=right
-            while (first-1)>=0:
-                if nums[first-1]==target:
-                    first=first-1
-                else:
-                    break
-            while last+1<=len(nums)-1:
-                if nums[last+1] == target:
-                    last = last+1
-                else:
-                    break
-            return [first,last]
-        
-        return [-1,-1]
+            return self.find_first_and_last(right, nums, target)
+
+        return [-1, -1]
+
+    def find_first_and_last(self, index, nums, target):
+        first = index
+        last = index
+        while (first - 1) >= 0:
+            if nums[first - 1] == target:
+                first = first - 1
+            else:
+                break
+        while last + 1 <= len(nums) - 1:
+            if nums[last + 1] == target:
+                last = last + 1
+            else:
+                break
+        return [first, last]
 
 
 # solution = Solution()
