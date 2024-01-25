227b55101519e8e7af4185331da789a6e56c45a0
Karan Rajbhar <kran@outlook.com>
2024-01-25 23:22:23 +0530

    updated problem number 4 median of two sorted array

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -8,24 +8,33 @@
 from typing import List
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-            finalList = nums1 + nums2
-            finalList = sorted(finalList)
-            
-            return self.findMiddle(finalList)
+        A=nums1
+        B=nums2
+        total = len(A) + len(B)
+        half = total // 2
+        if len(A) > len(B):
+                A, B = B, A
 
-    def findMiddle(self, numList):
-        if not numList :
-                return "The List is empty"
-        length = len(numList)
-            
-        if length % 2 != 0:
-                middleIndex = length//2
-                return numList[middleIndex]
+        l, r = 0, len(A) - 1
+
+        while True:
+                i = (l + r) // 2
+                j = half - i - 2
         
-        firstMiddleIndex = length //2-1
-        secondMiddleIndex = length//2
+                Aleft = A[i] if i >= 0 else float("-inf")
+                Aright = A[i+1] if (i+1) < len(A) else float("inf")
+                Bleft = B[j] if j >= 0 else float("-inf")
+                Bright = B[j+1] if (j+1) < len(B) else float("inf")
+        
+                if Aleft <= Bright and Bleft <= Aright:
+                        if total % 2:
+                                return min(Aright, Bright)
+                        return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+                elif Aleft > Bright:
+                        r = i - 1
+                else:
+                        l = i + 1
 
-        return (numList[firstMiddleIndex]+numList[secondMiddleIndex])/2                  
                    
 
         
