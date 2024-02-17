572c3182c637e22ebef0b207c8e12b80520e041f
Karan Rajbhar <kran@outlook.com>
2024-02-17 13:16:08 +0530

    solved problem number 424

--- 424.longest-repeating-character-replacement.py
+++ 424.longest-repeating-character-replacement.py
@@ -7,6 +7,16 @@
 # @lc code=start
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        count = [0] * 26
+        max_count = 0
+        start = 0
+        for end in range(len(s)):
+            count[ord(s[end]) - ord('A')] += 1
+            max_count = max(max_count, count[ord(s[end]) - ord('A')])
+            if end - start + 1 - max_count > k:
+                count[ord(s[start]) - ord('A')] -= 1
+                start += 1
+        return end - start + 1
         
 # @lc code=end
 
