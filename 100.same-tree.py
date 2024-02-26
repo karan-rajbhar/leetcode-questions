68685f638af7f2661159ffb78c2dfd2cbc40b2b4
Karan Rajbhar <kran@outlook.com>
2024-02-26 07:59:23 +0530

    added iterative solution using queue for problem number 100 same tree

--- 100.same-tree.py
+++ 100.same-tree.py
@@ -13,7 +13,7 @@
 #         self.right = right
 # Definition for a binary tree node.
 from typing import Optional
-
+from collections import deque
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -21,17 +21,42 @@ class TreeNode:
         self.right = right
 
 class Solution:
+
+    #Recursive solution
+    # def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    #     if p is None and q is None:
+    #         return True 
+
+    #     if p is None or q is None:
+    #         return False
+
+    #     if p.val != q.val:
+    #         return False
+
+
+    #     return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)        
+    
+    #Solution using queue in iterative way
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p is None and q is None:
-            return True 
+        queue = deque()
+        queue.append((p,q))
 
-        if p is None or q is None:
-            return False
+        
+        while queue:
+            p,q = queue.popleft()
 
-        if p.val != q.val:
-            return False
+            if p is None and q is None:
+                continue 
 
+            elif p is None or q is None:
+                return False
 
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)        
+            elif p.val != q.val:
+                return False
+            else:
+                queue.append((p.left,q.left))
+                queue.append((p.right,q.right))
+        
+        return True
 # @lc code=end
 
