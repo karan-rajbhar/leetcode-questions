f86a3f173d58612cd25d6b6ff45bb2b55f173ff8
Karan Rajbhar <kran@outlook.com>
2024-03-02 22:29:09 +0530

    added cleaner solution

--- 402.remove-k-digits.py
+++ 402.remove-k-digits.py
@@ -4,29 +4,29 @@
 # [402] Remove K Digits
 #
 
+
 # @lc code=start
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        stack=[]
+        stack = []
         for char in num:
-            while k > 0 and stack and stack[-1] > char:
-                k -=1
-                stack.pop()
-            
-            stack.append(char)
-        
-        stack = stack[:len(stack)-k]
-        result =[]
-        last_leading_zero=True
-        for char in stack:
-            if char == '0' and last_leading_zero:
-                continue
-            
-            last_leading_zero=False
-            result.append(char)
-        if result==[]:
-            return "0"
-        return "".join(result)
-        
-# @lc code=end
 
+            while stack and stack[-1] > char and k > 0:
+                k -= 1
+                stack.pop()
+
+            stack.append(char)
+
+        stack = stack[: len(stack) - k]
+        index = 0
+        for i, char in enumerate(stack):
+            if stack[i] != "0":
+                index = i
+                break
+            if i == len(stack) - 1:
+                index = i
+
+        return "".join(stack[index:]) if stack else "0"
+
+
+# @lc code=end
