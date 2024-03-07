750d7ab4257ff32132c2a0990e6034a2a7e2824b
Karan Rajbhar <kran@outlook.com>
2024-03-07 07:54:49 +0530

    added solution for the problem 876

---
+++ 876.middle-of-the-linked-list.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=141 lang=python3
+#
+# [141] Linked List Cycle
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        current=head
+        while current and head and head.next:
+            current=current.next
+            head=head.next.next
+        
+        return current
+# @lc code=end
+
