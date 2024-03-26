dccb5acfc2e2e7ebdceec4da252044b6a6d60452
Karan Rajbhar <kran@outlook.com>
2024-03-26 15:55:51 +0530

    added better solution for problem number 41

--- 41.first-missing-positive.py
+++ 41.first-missing-positive.py
@@ -6,15 +6,39 @@
 from typing import List
 # @lc code=start
 class Solution:
+    
+    # #Time complexity: O(nlogn) Space complexity: O(1)
+    # def firstMissingPositive(self, nums: List[int]) -> int:
+    #     smallest_int=1
+    #     nums.sort()
+    #     for num in nums:
+    #         if num == smallest_int:
+    #             smallest_int +=1
+        
+    #     return smallest_int
+    
+    #Time complexity: O(n) Space complexity: O(1)
+
     def firstMissingPositive(self, nums: List[int]) -> int:
-        smallest_int=1
-        nums.sort()
-        for num in nums:
-            if num == smallest_int:
-                smallest_int +=1
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def swap(i,j):
+            nums[i] , nums[j] = nums[j] , nums[i]
+
+        n = len(nums)
+        for i in range(n):
+            while 1<= nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+                swap(i, nums[i]-1)
+
         
-        return smallest_int
+        for i in range(n):
+            if i+1 != nums[i]:
+                return i+1
         
+        return n+1
+
 # @lc code=end
 
 # solution= Solution()
